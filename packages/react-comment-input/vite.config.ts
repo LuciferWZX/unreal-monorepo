9bd0131691160acdc1bca08b27f8ed7b7af4cf4e
wuzhixin <zhixin.wu@xbotspace.com>
2024-03-04 16:53:52 +0800

    feat:modify keywords and version

--- packages/react-comment-input/vite.config.ts
+++ packages/react-comment-input/vite.config.ts
@@ -24,14 +24,14 @@ export default defineConfig({
 
     // TypeError: Cannot read properties of undefined (reading '__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED')
     rollupOptions: {
-      // external: ['react', 'react-dom'],
-      // output: {
-      //   //在UMD构建模式下为这些外部化的依赖提供一个全局变量
-      //   globals: {
-      //     react: 'React',
-      //     'react-dom': 'ReactDOM',
-      //   },
-      // },
+      external: ['react', 'react-dom'],
+      output: {
+        //在UMD构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
     },
     outDir: 'lib',
   },
