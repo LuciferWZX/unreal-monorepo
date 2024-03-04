7f2b816340cf2c4a746098ddb23779fc15fa43f4
wuzhixin <zhixin.wu@xbotspace.com>
2024-03-04 16:38:51 +0800

    feat:modify keywords and version

--- packages/react-comment-input/vite.config.ts
+++ packages/react-comment-input/vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import { join, resolve } from 'path';
 import react from '@vitejs/plugin-react';
-// import dts from 'vite-plugin-dts';
+import dts from 'vite-plugin-dts';
 export default defineConfig({
   plugins: [
     react(),
-    // dts({
-    //   insertTypesEntry: true,
-    //   outDir: 'lib/types',
-    // }),
+    dts({
+      insertTypesEntry: true,
+      outDir: 'lib/types',
+    }),
   ],
   resolve: {
     alias: {
@@ -21,15 +21,17 @@ export default defineConfig({
       name: 'react-comment-input',
       fileName: (format) => `react-comment-input.${format}.js`,
     },
+
+    // TypeError: Cannot read properties of undefined (reading '__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED')
     rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        //在UMD构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
+      // external: ['react', 'react-dom'],
+      // output: {
+      //   //在UMD构建模式下为这些外部化的依赖提供一个全局变量
+      //   globals: {
+      //     react: 'React',
+      //     'react-dom': 'ReactDOM',
+      //   },
+      // },
     },
     outDir: 'lib',
   },
