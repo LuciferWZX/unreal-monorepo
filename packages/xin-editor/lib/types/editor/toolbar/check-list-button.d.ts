f17911c923b90405a28e56d73f71af31dfc9d5b7
wuzhixin <zhixin.wu@xbotspace.com>
2024-05-13 19:41:39 +0800

    fixed:更新了组件

---
+++ packages/xin-editor/lib/types/editor/toolbar/check-list-button.d.ts
@@ -0,0 +1,3 @@
+import { FC } from 'react';
+declare const CheckListButton: FC;
+export default CheckListButton;
---
+++ packages/xin-editor/lib/types/editor/toolbar/underline-button.d.ts
@@ -0,0 +1,3 @@
+import { FC } from 'react';
+declare const UnderlineButton: FC;
+export default UnderlineButton;
